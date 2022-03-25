63cc64965f5984c3ceeffeadb3ff9f3e78c6ab13
julio mata <julioematasv@gmail.com>
2022-03-25 14:42:55 +0100

    Creada estructura de carpetas

---
+++ cms/config/bd.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+  'driver' => 'mysql',
+  'host' => 'localhost',
+  'usuario' => 'homestead',
+  'password' => 'SeCret/123*'
+  'database' => 'cms',
+  charset' => 'utf8',
+);
