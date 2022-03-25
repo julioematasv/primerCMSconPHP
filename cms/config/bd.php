6e573bc0f7f0a0b3ce98d7b2073bc2d3df5afa4b
julio mata <julioematasv@gmail.com>
2022-03-25 14:51:38 +0100

    Cambios en archivo bd.php

--- cms/config/bd.php
+++ cms/config/bd.php
@@ -1,10 +1,17 @@
 <?php
 
 return array(
+
   'driver' => 'mysql',
+
   'host' => 'localhost',
+
   'usuario' => 'homestead',
+
   'password' => 'SeCret/123*'
+
   'database' => 'cms',
-  charset' => 'utf8',
+
+  charset' => 'utf8'
+  
 );
